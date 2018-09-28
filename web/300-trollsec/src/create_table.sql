1b1b2bbed18f268d67adbdec3b083152c8bae577
fdcarl <fdcarl@fdcarl.com>
2018-09-27 20:05:07 -0700

    Adding new 300 level web challenge

---
+++ web/300-trollsec/src/create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE secret (id Int, letter TEXT);
+INSERT INTO secret VALUES (1, 'd');
+INSERT INTO secret VALUES (2, 'e');
+INSERT INTO secret VALUES (3, 'a');
+INSERT INTO secret VALUES (4, 'd');
+INSERT INTO secret VALUES (5, 'b');
+INSERT INTO secret VALUES (6, 'e');
+INSERT INTO secret VALUES (7, 'e');
+INSERT INTO secret VALUES (8, 'f');
+
+CREATE TABLE trolls (id Int, url TEXT);
+INSERT INTO trolls VALUES (1, "https://i.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg");
+INSERT INTO trolls VALUES (2, "https://i2.silvergames.com/p/b/trollface-quest.png");
+INSERT INTO trolls VALUES (3, "https://static.webshopapp.com/shops/027322/files/157633856/trollface-mask.jpg");
+INSERT INTO trolls VALUES (4, "https://i.ytimg.com/vi/kcMt_Rc6ZLs/maxresdefault.jpg");
