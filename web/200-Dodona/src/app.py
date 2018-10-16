28e166cfbe569d5046658d091cdaa514b2102c6c
fdcarl <fdcarl@fdcarl.com>
2018-10-15 21:20:22 -0700

    Adding documentation

--- web/200-Dodona/src/app.py
+++ web/200-Dodona/src/app.py
@@ -12,14 +12,13 @@ app = Flask(__name__)
 
 IV = Random.new().read(AES.block_size)
 
-#KEY = hashlib.sha256("testing").digest()
-
 random.seed()
 
 KEY = ''.join(chr(random.randint(0,255)) for i in range(16))
 
 PADDING_STYLE = "pkcs7"
 
+# Default cookie data
 STARTING_COOKIE_DATA = {'username': 'guest', 'whats_the_answer_to_life_the_universe_and_everything': '', 'security_put_some_text_here': ''}
 COOKIE_NAME = "wisdom_of_the_gods"
 
@@ -30,7 +29,8 @@ def index():
 
     has_query = False
     answer = ''
-
+    
+    # Completely pointless, just a little fun..
     if "q" in request.args:
         has_query = True
         if "flag" in request.args["q"].lower():
@@ -38,11 +38,14 @@ def index():
         else:
             answer = magic_8_ball()
 
+    # If user doesn't have a cookie set, set them up with one
     if current_cookie is None:
         encrypted_cookie = encrypt_cookie(STARTING_COOKIE_DATA)
         resp = make_response(render_template('index.html', query=has_query))
         resp.set_cookie(COOKIE_NAME, encrypted_cookie)
         return resp
+
+    # Otherwise, attempt to decrypt the cookie. Gives useful info during padding oracle attack.
     else:
         decrypted_cookie = decrypt_cookie(current_cookie)
 
@@ -50,6 +53,7 @@ def index():
 
         return resp
 
+# If correct cookie has been set, show the user the flag. Otherwise, they get a 403 Forbidden as a hint there is something here
 @app.route("/admin")
 def admin():
     solved = False
@@ -60,7 +64,6 @@ def admin():
         solved = check_cookie(decrypted_cookie)
 
     if not solved:
-        #return render_template('admin_fail.html')
         abort(403)
     else:
         return render_template('admin.html')
@@ -92,7 +95,7 @@ def magic_8_ball():
     return random.choice(answers)
 
 
-
+# Encrypt data for the cookie
 def encrypt_cookie(cookie_data):
     cookie_cipher = AES.new(KEY, AES.MODE_CBC, IV)
     padded = Padding.pad(json.dumps(cookie_data), AES.block_size, style=PADDING_STYLE)
@@ -100,9 +103,11 @@ def encrypt_cookie(cookie_data):
 
     return encrypted_cookie
 
+# Attempt to decrypt the cookie
 def decrypt_cookie(encrypted_cookie):
     cookie_cipher = AES.new(KEY, AES.MODE_CBC, IV)
     
+    # Cookie decryption may fail if user has messed with the data
     try:
         b64_decoded_cookie = base64.b64decode(urllib.unquote(encrypted_cookie))
         decrypted_cookie = cookie_cipher.decrypt(b64_decoded_cookie)
@@ -110,11 +115,16 @@ def decrypt_cookie(encrypted_cookie):
         unpadded_cookie = unpadded_cookie[len(IV):] #remove beginning IV
         return unpadded_cookie
 
+    # If cookie decryption fails for any reason, throw up a 500. Gives useful info to user during padding oracle attack
     except Exception, e:
         print str(e)
         abort(500)
 
 
+# Check if the cookie has all the correct data to solve the challenge
+# Username should be 'admin'
+# whats_the_answer_to_life_the_universe_and_everything should be 42
+# and security_put_some_text_here should just contain something
 def check_cookie(cookie_data):
     cookie = json.loads(cookie_data)
 
@@ -130,6 +140,8 @@ def check_cookie(cookie_data):
     return False
 
 
+# Useful feature during debugging so I didn't have to manually remove the cookie every time :)
+# will leave in just in case it's useful during the CTF
 @app.route("/clear_cookie")
 def clear_cookie():
     resp = make_response()
